2d6b4561cd34aa460035cd157a4ee26b4db05fb8
daydash <thedaydash@gmail.com>
2023-04-21 13:14:14 +0530

    added meals section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+
 import Header from "./components/Layout/Header";
 
 function App() {
 	return (
-		<div>
+		<React.Fragment>
 			<Header />
-		</div>
+		</React.Fragment>
 	);
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import mealsImg from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
 
 const Header = (props) => {
 	return (
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>React Meals</h1>
-				<button>Cart</button>
+				<HeaderCardButton />
 			</header>
 			<div className={classes["main-image"]}>
 				<img src={mealsImg} alt="table full of delicious food" />
